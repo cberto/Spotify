1015fcda11f66c95ecf17beb5663b6c63b317254
cberto <bertolanicami@gmail.com>
2021-11-02 12:12:47 -0300

    avances

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+import { FavoritesModule } from '../favorites/favorites.module';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{
+  path: 'tracks',
+  loadChildren: () => import('@modules/tracks/tracks.module').then(m=> m.TracksModule)
+},
+{
+  path: 'favorites',
+  loadChildren: () => import('@modules/favorites/favorites.module').then(m=> m.FavoritesModule)
+},
+{
+  path: 'history',
+  loadChildren: () => import('@modules/history/history.module').then(m=> m.HistoryModule)
+}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
